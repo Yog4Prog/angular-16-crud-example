40ba801486517b7f8002765e6ed647503325598b
Yog4Prog <raju4techno@gmail.com>
2024-05-18 14:50:32 +0530

    Added puppeteer

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -42,7 +42,6 @@ module.exports = function (config) {
         flags: [
           '--no-sandbox', 
           '--disable-gpu',
-          '--headless',
           '--disable-translate',
           '--disable-extensions'
         ]
